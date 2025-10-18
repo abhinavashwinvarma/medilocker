0b96aa9761ede631a0cbbe9553020d23d2643fbe
Abhinav Varma <abhinavashwinvarma21@gmail.com>
2025-10-18 20:10:46 +0530

    UI updates

--- main.py
+++ main.py
@@ -7,26 +7,61 @@ from customtkinter import *
 
 user = Doctor()
 
-def say_hi():
-    print('hi')
-
 root = CTk() #application window
 root.title("CS Project")
-root.configure(fg_color = "#1D262F")
 root.geometry('800x500')
+root.configure(fg_color = "#1D262F")
+root.columnconfigure(0, weight = 1)
+root.rowconfigure(0, weight = 1)
 
+mainFrame = CTkFrame(root) #create frame object
+mainFrame.configure(fg_color = 'transparent', 
+                    border_width = 4, 
+                    border_color = "#2B3743")
+mainFrame.grid(column = 0, row = 0, sticky = 'NSEW') #place in (0, 0) of parent root, sticky implies to anchor
+mainFrame.grid_configure(padx = 20,
+                        pady = 20)
+mainFrame.columnconfigure(0, weight = 1)
+mainFrame.rowconfigure(1, weight = 1)
 
-mainframe = CTkFrame(root, fg_color = 'transparent') #create frame object
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S)) #place in (0, 0) of parent root, sticky implies to anchor
+helloLabel = CTkLabel(mainFrame, text = 'Hi Anishwar!')
+helloLabel.configure(font = ('Poppins Regular', 50), 
+                     fg_color = 'transparent')
+helloLabel.grid(column = 0, row = 0, sticky = '')
+helloLabel.grid_configure(pady = 100)
 
-#ttk.Button(mainframe, text= 'Hi', command= say_hi).grid(column = 1, row = 1, sticky = W)
-helloLabel = CTkLabel(mainframe, text = 'Hi Anishwar.', font = ('Berlin Sans FB', 48), fg_color="transparent")
-helloLabel.grid(column = 2, row = 1, sticky = N)
-helloLabel.grid_configure(padx = 300, pady = 30)
+buttonFrame = CTkFrame(mainFrame)
+buttonFrame.configure(fg_color = 'transparent')
+buttonFrame.grid(column = 0, row = 1, sticky = 'NSEW') 
+buttonFrame.grid_configure(padx = 50, pady = 50)
+buttonFrame.columnconfigure(0, weight = 1)
 
-login = CTkButton(mainframe, fg_color = "#34FEBB", text = 'LOGIN', text_color = '#1D262F', font = ('Berlin Sans FB', 25), corner_radius = 999)
-login.configure(hover_color = "#1EC78F")
-login.grid(column = 2, row = 6, sticky = 'N')
+loginButton = CTkButton(buttonFrame, text = 'LOGIN')
+loginButton.configure(font = ('Lexend Giga Regular', 20),
+                      fg_color = 'transparent',
+                      text_color = '#FFFFFF',
+                      hover_color = '#14AE92',
+                      width = 300,
+                      height = 50,
+                      border_width = 4,
+                      border_color = '#14AE92',
+                      corner_radius = 10)
+loginButton.grid(column = 0, row = 0, sticky = 'EW')
+loginButton.grid_configure(pady = 20)
+
+signupButton = CTkButton(buttonFrame, text = 'CREATE ACCOUNT')
+signupButton.configure(font = ('Lexend Giga Regular', 20),
+                      fg_color = 'transparent',
+                      text_color = '#FFFFFF',
+                      hover_color = '#14AE92',
+                      width = 300,
+                      height = 50,
+                      border_width = 4,
+                      border_color = '#14AE92',
+                      corner_radius = 10)
+signupButton.grid(column = 0, row = 1, sticky = 'EW')
+signupButton.grid_configure(pady = 20)
+
 root.mainloop()
 
 #user.login('Pranav Vijay', 'EddyHater123')
