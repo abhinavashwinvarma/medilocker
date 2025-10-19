910658a655efd0dab74d8026b57e8f0c9bd40732
Abhinav Varma <abhinavashwinvarma21@gmail.com>
2025-10-19 17:47:14 +0530

    Added theme JSONs

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from tkinter import *
 from tkinter import ttk
 from customtkinter import *
 
-user = Doctor()
+user = Patient()
+
+set_default_color_theme("ColorThemes\lightmode.json")
 
 root = CTk() #application window
 root.title("MediLocker")
 root.geometry('800x500')
-root.configure(fg_color = "#443B81")
 root.columnconfigure(0, weight = 1)
 root.rowconfigure(0, weight = 1)
+
+current_theme = 'dark'
 
 def load_mainpage():
 
     mainFrame = CTkFrame(root) #create frame object
-    mainFrame.configure(fg_color = 'transparent', 
-                        border_width = 4, 
-                        border_color = "#2E294E")
     mainFrame.grid(column = 0, row = 0, sticky = 'NSEW') #place in (0, 0) of parent root, sticky implies to anchor
     mainFrame.grid_configure(padx = 20,
                             pady = 20)
@@ -27,138 +27,104 @@ def load_mainpage():
     mainFrame.rowconfigure(1, weight = 1)
 
     helloLabel = CTkLabel(mainFrame, text = 'Welcome.\nSign in to continue.')
-    helloLabel.configure(font = ('Libre Caslon Text Regular', 50), 
-                        fg_color = 'transparent',
-                        text_color = "#FFFFFF")
+    helloLabel.configure(font = ('Libre Caslon Text Regular', 50))
     helloLabel.grid(column = 0, row = 0, sticky = '')
     helloLabel.grid_configure(pady = 100)
 
     buttonFrame = CTkFrame(mainFrame)
-    buttonFrame.configure(fg_color = 'transparent')
+    buttonFrame.configure(border_width = 0)
     buttonFrame.grid(column = 0, row = 1, sticky = 'NSEW') 
     buttonFrame.grid_configure(padx = 50, pady = 50)
     buttonFrame.columnconfigure(0, weight = 1)
 
     loginButton = CTkButton(buttonFrame, text = 'SIGN IN', command = load_signinpage)
     loginButton.configure(font = ('Lexend Giga Regular', 20),
-                        fg_color = 'transparent',
-                        text_color = '#FFFFFF',
-                        hover_color = '#DE9E36',
                         width = 300,
-                        height = 50,
-                        border_width = 4,
-                        border_color = '#DE9E36',
-                        corner_radius = 10)
+                        height = 50)
     loginButton.grid(column = 0, row = 0, sticky = 'EW')
     loginButton.grid_configure(pady = 20)
 
     signupButton = CTkButton(buttonFrame, text = 'CREATE ACCOUNT')
     signupButton.configure(font = ('Lexend Giga Regular', 20),
-                        fg_color = 'transparent',
-                        text_color = '#FFFFFF',
-                        hover_color = '#DE9E36',
                         width = 300,
-                        height = 50,
-                        border_width = 4,
-                        border_color = '#DE9E36',
-                        corner_radius = 10)
-
+                        height = 50)
     signupButton.grid(column = 0, row = 1, sticky = 'EW')
     signupButton.grid_configure(pady = 20)
+
+    mainFrame.lift()
 
 def load_signinpage():
 
     signinFrame = CTkFrame(root) #create frame object
-    signinFrame.configure(fg_color = 'transparent', 
-                        border_width = 4, 
-                        border_color = "#2E294E")
     signinFrame.grid(column = 0, row = 0, sticky = 'NSEW') #place in (0, 0) of parent root, sticky implies to anchor
     signinFrame.grid_configure(padx = 20,
                             pady = 20)
     signinFrame.columnconfigure(0, weight = 1)
-    signinFrame.rowconfigure(1, weight = 1)
-    signinFrame.rowconfigure(1, weight = 1)
+    signinFrame.rowconfigure(2, weight = 1)
 
     signinLabel = CTkLabel(signinFrame, text = 'Sign in')
-    signinLabel.configure(font = ('Libre Caslon Text Regular', 50), 
-                        fg_color = 'transparent',
-                        text_color = "#FFFFFF")
-    signinLabel.grid(column = 0, row = 0, sticky = 'W')
-    signinLabel.grid_configure(pady = 50,
+    signinLabel.configure(font = ('Libre Caslon Text Regular', 50))
+    signinLabel.grid(column = 0, row = 1, sticky = 'W')
+    signinLabel.grid_configure(pady = (50, 50),
                                padx = 50)
     
+    exitButton = CTkButton(signinFrame, text = '← BACK', command = load_mainpage)
+    exitButton.configure(font = ('Lexend Giga Regular', 20),
+                         fg_color = 'transparent',
+                         width = 20,
+                         height = 20)
+    exitButton.grid(column = 0, row = 0, sticky = 'W')
+    exitButton.grid_configure(pady = (50, 0),
+                              padx = 50)
+
     fillupFrame = CTkFrame(signinFrame)
-    fillupFrame.configure(fg_color = 'transparent')
-                        #border_width = 4, 
-                        #border_color = "#2E294E")
-    fillupFrame.grid(column = 0, row = 1, sticky = 'NSEW') #place in (0, 0) of parent root, sticky implies to anchor
+    fillupFrame.configure(fg_color = 'transparent',
+                        border_width = 0)
+    fillupFrame.grid(column = 0, row = 2, sticky = 'NSEW') #place in (0, 0) of parent root, sticky implies to anchor
     fillupFrame.grid_configure(padx = 20,
                             pady = 20)
-
     fillupFrame.columnconfigure(0, minsize = 300)
     fillupFrame.columnconfigure(1, weight = 1)
 
     userLabel = CTkLabel(fillupFrame, text = 'Username')
-    userLabel.configure(font = ('Libre Caslon Text Regular', 25), 
-                        fg_color = 'transparent',
-                        text_color = "#FFFFFF")
+    userLabel.configure(font = ('Libre Caslon Text Regular', 25))
     userLabel.grid(column = 0, row = 0, sticky = 'W')
     userLabel.grid_configure(pady = 20,
                             padx = 30)
     
     userEntry = CTkEntry(fillupFrame, placeholder_text = 'Enter your username...')
     userEntry.configure(font = ('Libre Caslon Text Regular', 20),
-                        fg_color = '#2E294E',
-                        text_color = '#FFFFFF', 
-                        placeholder_text_color = '#DE9E36',
                         width = 300,
-                        height = 50,
-                        border_width = 4,
-                        border_color = '#DE9E36',
-                        corner_radius = 10)
+                        height = 50)
     userEntry.grid(column = 1, row = 0, sticky = 'EW')
     userEntry.grid_configure(pady = 20,
                             padx = 20)
+    userEntry.focus()
     
 
     passwordLabel = CTkLabel(fillupFrame, text = 'Password')
-    passwordLabel.configure(font = ('Libre Caslon Text Regular', 25), 
-                        fg_color = 'transparent',
-                        text_color = "#FFFFFF")
+    passwordLabel.configure(font = ('Libre Caslon Text Regular', 25))
     passwordLabel.grid(column = 0, row = 1, sticky = 'W')
     passwordLabel.grid_configure(pady = 20,
                                 padx = 30)
     
     passwordEntry = CTkEntry(fillupFrame, placeholder_text = 'Enter your password...')
     passwordEntry.configure(font = ('Libre Caslon Text Regular', 20),
-                        fg_color = '#2E294E',
-                        text_color = '#FFFFFF', 
-                        placeholder_text_color = '#DE9E36',
                         width = 300,
-                        height = 50,
-                        border_width = 4,
-                        border_color = '#DE9E36',
-                        corner_radius = 10)
+                        height = 50)
     passwordEntry.grid(column = 1, row = 1, sticky = 'EW')
     passwordEntry.grid_configure(pady = 20,
                                  padx = 20)
     
     signinButton = CTkButton(fillupFrame, text = 'SIGN IN', command = lambda: user.login(userEntry.get(), passwordEntry.get()))
     signinButton.configure(font = ('Lexend Giga Regular', 20),
-                        fg_color = 'transparent',
-                        text_color = '#FFFFFF',
-                        hover_color = '#DE9E36',
                         width = 200,
-                        height = 50,
-                        border_width = 4,
-                        border_color = '#DE9E36',
-                        corner_radius = 10)
+                        height = 50)
     signinButton.grid(column = 1, row = 3, sticky = 'E')
     signinButton.grid_configure(pady = 20,
                                padx = 20)
     
     signinFrame.lift()
-
 
 load_mainpage()
 
